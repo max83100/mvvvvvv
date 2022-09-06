367a82e6d63f9ee991bba3e99de4d419d296e867
max83100 <gguvggvhj017@gmail.com>
2022-09-06 21:13:00 +0300

    06.09

--- app/src/main/java/com/example/mvm/MainActivity.kt
+++ app/src/main/java/com/example/mvm/MainActivity.kt
@@ -2,15 +2,33 @@ package com.example.mvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val getData = GetData()
+    private var liveDatastring = MutableLiveData<String>()
+    val myLiveData = MyLiveData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lifecycle.addObserver(getData)
+        myLiveData.observe(this, Observer {
+            test_text.text = it
+        })
+
+        button.setOnClickListener{
+            myLiveData.setValueTo(editText.text.toString())
+        }
+
+
     }
 
 
 }
+
+
